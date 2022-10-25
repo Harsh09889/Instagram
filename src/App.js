d0c9db130988152104fa2cc7e5775190dbd4882b
Harsh <harsh09889@gmail.com>
2022-10-25 17:14:54 +0530

    firebase installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Navbar } from './components/Navbar';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Navbar></Navbar>
+			<h1>This is heading and also a hello from Harsh to Annu...</h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+	return <p>this is a hello from navbar</p>;
+}
+
+function dusraFunction() {
+	return 'abcd';
+}
+
+export { dusraFunction, Navbar };
+// export default Navbar;
